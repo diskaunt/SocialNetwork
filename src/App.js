c3820bdf532689c38838274ca75a24cd8ddf4c8a
Mikhail B <diskauntm@gmail.com>
2024-07-04 15:45:13 +0300

    Repare

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import "./App.css";
 import React, { useEffect } from "react";
-import {
-  Outlet,
-} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import { connect } from "react-redux";
 import { initializedApp } from "./redux/app-reduser";
 import CirclePreloader from "./components/common/preloader/CirclePrelloader";
 
-function App(props) {
-	useEffect(() => {
-    props.initializedApp();
-  }, [props.initialized]);
+function App({ initializedApp, initialized, ...props }) {
+  useEffect(() => {
+    initializedApp();
+  }, [initialized]);
   return (
-	<div className="app-wrapper">
-	<HeaderContainer />
-	{props.initialized? <><Sidebar />
-	<div className="appWrapperContent">
-		<Outlet />
-	</div></> : <CirclePreloader />}
-</div>
-	)
+    <div className="app-wrapper">
+      <HeaderContainer />
+      {initialized ? (
+        <>
+          <Sidebar />
+          <div className="appWrapperContent">
+            <Outlet />
+          </div>
+        </>
+      ) : (
+        <CirclePreloader />
+      )}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
@@ -31,4 +35,4 @@ const mapStateToProps = (state) => ({
 
 const ReactJsApp = connect(mapStateToProps, { initializedApp })(App);
 
-export default ReactJsApp
+export default ReactJsApp;
