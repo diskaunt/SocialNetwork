65b45aeb66a7cbf8306eda2d7cd4e6a145ffa365
Mikhail B <diskauntm@gmail.com>
2024-07-05 13:57:48 +0300

    homepage

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/redux-store";
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import NoMatch from "./components/ErrorPage/NoMatch";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import LoginPage from "./components/Login/Login";
+import { lazy } from "react";
+import Preloader from "./components/common/preloader/Preloader";
+import { withSuspense } from "./hoc/withSuspense";
+
+const DialogsContainer = lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = lazy(() =>
+  import("././components/Profile/ProfileContainer")
+);
+
+const UsersContainer = lazy(() => import("./components/Users/UsersContainer"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/SocialNetwork/",
+    element: <App />,
+    errorElement: <NoMatch />,
+    children: [
+      {
+        path: "/profile/:userId?",
+        element: withSuspense(ProfileContainer)(),
+        errorElement: <NoMatch />,
+      },
+      {
+        path: "dialogs/*",
+        element: withSuspense(DialogsContainer)(),
+        errorElement: <NoMatch />,
+      },
+      { path: "news/", element: <News /> },
+      { path: "music/", element: <Music /> },
+      { path: "settings/", element: <Settings /> },
+      {
+        path: "users/",
+        element: withSuspense(UsersContainer)(),
+        errorElement: <NoMatch />,
+      },
+      { path: "login", element: <LoginPage /> },
+    ],
+  },
+]);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
